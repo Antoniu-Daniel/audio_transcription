257216625c4123614c0dec048b8549db79696979
Danielzapirtan <137394552+Danielzapirtan@users.noreply.github.com>
2025-03-31 22:14:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ import whisper
 import time
 import os
 
-def transcribe(audio, model_size):
+def transcribe(audio, is_not_robot, model_size):
+					if is_not_robot==False:
+        return "Error: please check that you are not a psychiatrist"
     start_time = time.time()
     model = whisper.load_model(model_size)
     result = model.transcribe(audio)
@@ -19,7 +21,7 @@ def transcribe(audio, model_size):
 
 gui = gr.Interface(
     fn=transcribe,
-    inputs=[gr.Audio(type="filepath"), gr.Dropdown(["medium", "large"], label="Model Size")],
+    inputs=[gr.Audio(type="filepath"),  robot_checkbox = gr.Checkbox(label="I am not a psychiatrist"), gr.Dropdown(["medium", "large"], label="Model Size")],
     outputs=[gr.Textbox(label="Transcript"), gr.File(label="Download TXT"), gr.Textbox(label="Elapsed Time")],
     title="Whisper Transcription",
     description="Upload an audio file and choose a model size to transcribe it."
